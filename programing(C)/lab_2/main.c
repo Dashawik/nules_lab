fd50673bf16b48967fded6c2b4c85e0abe90c39d
Dashawik <piatenkodasha@gmail.com>
2023-09-14 22:40:24 +0300

    lab_2

---
+++ programing(C)/lab_2/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int
+main ()
+{
+    float x;
+    printf("Enter x\n");
+    scanf("%f",&x);
+
+    if (x>10)
+        printf("error");
+    if (x<10)
+        if (x>=1)
+            printf("y=%f",x*x*x -2);
+    if (x<0)
+        if (x>-5)
+            printf("y=%f",x*x +5);
+    if (x<-5)
+        printf("y=%f",x*x*x -2);
+
+
+
+    return 0;
+
+}
