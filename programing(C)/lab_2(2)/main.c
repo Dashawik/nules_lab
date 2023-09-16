451f3d643cb4c87bb328bcf6c3bf1f5c0b2172a1
Dashawik <piatenkodasha@gmail.com>
2023-09-16 10:48:18 +0300

    lab2_2(исправлено)
    This reverts commit 82af5e58b409aa800145df08602e5c3394e3c42e.

--- programing(C)/lab_2(2)/main.c
+++ programing(C)/lab_2(2)/main.c
@@ -5,10 +5,10 @@ int main() {
     printf("Введіть x\n");
     scanf("%f",&x);
     if (x>10)
-        printf("ERROR!!!\n");
-        if ( (x<-5)|| ((x>=1)&&(x<=10)))
-            printf("y=%f",x*x*x-2);
-            if ((x>-5)&&(x<=0))
-                printf("y=%f",x*x+5);
+        printf("ERROR!!!");
+    if ((x<-5))
+        printf("y=%f",x*x+5);
+    if (  (x>-5)|| ((x>=1)&&(x<10)))
+        printf("y=%f",x*x*x-2);
     return 0;
 }
