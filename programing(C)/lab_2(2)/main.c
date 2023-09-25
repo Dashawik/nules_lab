27d7a683503ab181d9fa307e6d5f3e9e8def2c13
Dashawik <piatenkodasha@gmail.com>
2023-09-25 21:52:20 +0300

    переделана до правильных вычилений

--- programing(C)/lab_2(2)/main.c
+++ programing(C)/lab_2(2)/main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    float x;
+    int x;
     printf("Введіть x\n");
-    scanf("%f",&x);
+    scanf("%d",&x);
     if (x>10)
         printf("ERROR!!!");
-    if ((x<-5))
-        printf("y=%f",x*x+5);
-    if (  (x>-5)|| ((x>=1)&&(x<10)))
-        printf("y=%f",x*x*x-2);
+    if (  (x<=-5)|| ((x>=1)&&(x<=10)))
+        printf("y=%d",x*x*x-2);
+    if ((x>-5)&& (x<=0))
+        printf("y=%d",x*x+5);
+    if (x>10)
+        printf("ERROR!!!");
     return 0;
 }
