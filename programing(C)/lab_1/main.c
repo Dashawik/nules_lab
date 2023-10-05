548af82a4dc64a98a442efeee1940d8110a3aa3e
Dashawik <piatenkodasha@gmail.com>
2023-10-05 17:33:35 +0300

    lab1_zavdan2(корректно обч дробн числа)

--- programing(C)/lab_1/main.c
+++ programing(C)/lab_1/main.c
@@ -13,7 +13,7 @@ int main() {
 
 
     float t1 = a*b*y/3 + d/x *log(a*x +y);
-    float t2 =1/2 *(log(y/x))-a*b/(2*y);
+    float t2 =1.0/2 *(log(y/x))-a*b/(2*y);
 
     printf("Значення t1: %f\n", t1);
     printf("Значення t2: %f\n", t2);
