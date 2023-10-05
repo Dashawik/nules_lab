53279ea741755325ecf7ff1c91663a877f99e834
Dashawik <piatenkodasha@gmail.com>
2023-10-05 17:31:15 +0300

    lab2_zavdan(без логічн опер)

--- programing(C)/lab_2/main.c
+++ programing(C)/lab_2/main.c
@@ -2,20 +2,25 @@
 
 int main ()
 {
-    int x;
+    float x;
     printf("Введіть x\n");
-    scanf("%d",&x);
+    scanf("%f",&x);
 
     if (x>10)
         printf("ERROR!!!");
+    if (x==-5)
+        printf("ERROR!!!");
+    if (x>=0)
+        if(x<1)
+            printf("ERROR!!!");
     if (x<-5)
-        printf("y=%d",x*x*x -2);
+        printf("y=%f",x*x*x -2);
     if (x>=1)
         if (x<=10)
-            printf("y=%d",x*x*x -2);
+            printf("y=%f",x*x*x -2);
     if (x>-5)
-        if (x<=0)
-            printf("y=%d",x*x +5);
+        if (x<0)
+            printf("y=%f",x*x +5);
     return 0;
 
 }
